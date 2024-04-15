42a36923e5e77950fc2ea327b9c403d44a5ba42b
KFH <KFH@RAZAN-ALSHATTI>
2024-04-15 09:42:39 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// C# OOP 
+// task 1 
+class Student
+{
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double GPA { get; set; }
+    
+    public Student(string name,string major,double gpa)
+    {
+        Name = name;
+        Major = major;
+        GPA = gpa;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Major: {Major}");
+        Console.WriteLine($"GPA: {GPA:F2}"); // formatted student summary 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student1 = new Student("Razan Alshatti", "Computer engineer", 3.5);
+
+        student1.DisplayInfo();
+    }
+}
+
